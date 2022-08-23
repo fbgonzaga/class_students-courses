6abdd44d32145206582a4f9d3fc8580111de6de1
PhiCross5 <felipe.choi@sou.unifal-mg.edu.br>
2022-08-22 21:20:52 -0300

    bean serviço curso com listagem e criação

--- src/main/java/com/school/management/service/CourseService.java
+++ src/main/java/com/school/management/service/CourseService.java
@@ -1,8 +1,18 @@
 package com.school.management.service;
 
 import com.school.management.model.Course;
+import com.school.management.model.Student;
+import com.school.management.model.dto.CourseDto;
+import com.school.management.model.dto.StudentDto;
 import com.school.management.repository.CourseRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CourseService {
@@ -12,7 +22,31 @@ public class CourseService {
     public CourseService(CourseRepository courseRepository){this.courseRepository = courseRepository;};
 
 
-//    public List<Course> getCourses(){
-//        return null;
-//    }
+    public List<CourseDto> getCourses(){
+        return courseRepository.findAll().stream()
+                .map(course -> new CourseDto(course.getId(), course.getName(), course.getCreatedAt(), course.getUpdatedAt()))
+                .collect(Collectors.toList());
+    }
+
+    public List<CourseDto> createcourses(List<CourseDto> coursesDto) {
+        if (coursesDto.size() > 50) {
+            throw new ResponseStatusException(
+                    HttpStatus.FORBIDDEN, "A request can not contain more than 50 courses.");
+        }
+
+        Timestamp ts = Timestamp.from(Instant.now());
+        List<Course> l = courseRepository.saveAll(coursesDto.stream()
+                .filter(s -> s.getName() != null && !s.getName().isBlank())
+                .map(courseDto -> new Course(courseDto.getId(),
+                        courseDto.getName(),
+                        ts,
+                        ts))
+                .collect(Collectors.toList()));
+        return l.stream()
+                .map(course -> new CourseDto(course.getId(),
+                        course.getName(),
+                        course.getCreatedAt(),
+                        course.getUpdatedAt()))
+                .collect(Collectors.toList());
+    }
 }
